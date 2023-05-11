1279ae5d11b0130db86ac85ce6978c0fcee2e24a
HunaisAshraf <hunaisashraf@gmail.com>
2023-05-11 18:54:54 +0530

    modified

--- frontend/src/components/pages/home.jsx
+++ frontend/src/components/pages/home.jsx
@@ -1,40 +1,164 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../layout/layout";
-import { useAuth } from "../../context/auth";
-import { useState, useEffect } from "react";
 import axios from "axios";
+import { Checkbox, Radio } from "antd";
+import { prices } from "../prices";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-hot-toast";
+import { useCart } from "../../context/cart";
 
-const Home = () => {
-  // const [auth, setAuth] = useAuth();
-
+const AllProducts = () => {
   const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [checked, setChecked] = useState([]);
+  const [radio, setRadio] = useState([]);
+  const [cart, setCart] = useCart([]);
+
+  const navigate = useNavigate();
+
+  const getProducts = async () => {
+    try {
+      const res = await axios.get("/api/get-product");
+      setProducts(res.data.products);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
-    let fetchData = async () => {
-      let response = await fetch("https://fakestoreapi.com/products/2");
-      let resData = await response.json();
-      setProducts(resData);
-    };
-    fetchData();
+    getProducts();
   }, []);
 
+  const getAllCategory = async () => {
+    try {
+      const res = await axios.get("/api/get-category");
+      if (res.data.success) {
+        setCategories(res.data.category);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (!checked.length || !radio.length) {
+      getAllCategory();
+    }
+  }, [checked.length, radio.length]);
+
+  //filter
+  const handleFilter = (value, id) => {
+    let all = [...checked];
+
+    if (value) {
+      all.push(id);
+    } else {
+      all = all.filter((c) => c !== id);
+    }
+    setChecked(all);
+    console.log(checked);
+  };
+
+  //get filtered products
+  const filteredProducts = async () => {
+    try {
+      const res = await axios.post("/api/product-filters", { checked, radio });
+      setProducts(res.data.products);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (checked.length || radio.length) {
+      filteredProducts();
+    }
+    //eslint-disable-next-line
+  }, [checked, radio]);
   return (
-    <Layout title={"Aech-Shop"}>
-      {/* <h1>Homepage</h1>
-      <div className="product">
-        <div className="card" style={{ width: "18rem" }}>
-          <img src={products.image} className="card-img-top" alt="..." />
-          <div className="card-body">
-            <p className="card-text">{products.title}</p>
-            <p className="card-text">{products.price}</p> */}
-            {/* <p>{products.rating.e}</p> */}
-            {/* <p className="card-text">{products.rating}</p> */}
-          {/* </div>
+    <Layout title={"All-products"}>
+      <div className="container-fluid row m-2">
+        <div className="col-md-2 mt-3">
+          {/* category filter */}
+
+          <h4>Filter by category</h4>
+          <div className="d-flex flex-column">
+            {categories.map((c) => (
+              <Checkbox
+                className="ms-2"
+                key={c._id}
+                onChange={(e) => handleFilter(e.target.checked, c._id)}
+              >
+                {c.name}
+              </Checkbox>
+            ))}
+          </div>
+
+          {/* price filter */}
+
+          <h4 className="mt-4">Filter by Price</h4>
+          <div className="d-flex flex-column">
+            <Radio.Group
+              className="ms-2"
+              onChange={(e) => setRadio(e.target.value)}
+            >
+              {prices.map((p) => (
+                <div key={p._id}>
+                  <Radio value={p.array}>{p.name}</Radio>
+                </div>
+              ))}
+            </Radio.Group>
+          </div>
+          <div className="mt-4 ms-4">
+            <button
+              className="btn btn-outline-danger"
+              onClick={() => window.location.reload()}
+            >
+              Reset Filter
+            </button>
+          </div>
         </div>
-      </div> */}
-      {/* <pre>{JSON.stringify(auth, null, 5)}</pre> */}
+        <div className="col-md-10">
+          <h1 className="text-center">All products</h1>
+          <div className="d-flex flex-wrap">
+            {products.map((product) => (
+              <div
+                className="card m-2 pt-2"
+                key={product._id}
+                style={{ width: "18rem" }}
+              >
+                <img
+                  src={`/api/product-photo/${product._id}`}
+                  className="card-img-top"
+                  alt="..."
+                />
+                <div className="card-body">
+                  <h6 className="card-title">{product.name}</h6>
+                  <p className="card-text">₹ {product.price}</p>
+                  <button
+                    className="btn btn-primary ms-2"
+                    onClick={() => navigate(`/product/${product.slug}`)}
+                  >
+                    More Details
+                  </button>
+                  <button
+                    className="btn btn-secondary ms-2"
+                    onClick={() => {
+                      setCart([...cart, product]);
+                      localStorage.setItem("cart",JSON.stringify([...cart,product]))
+                      toast.success("Item Added To Cart")
+                    }}
+                  >
+                    Add to cart
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
 
-export default Home;
+export default AllProducts;
